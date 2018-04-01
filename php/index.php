169c5b155d8b2d9e4f548d77722e849de0a951b2
Hannah An <baby999@uw.edu>
2018-04-01 14:33:37 -0400

    Update index.php

--- php/index.php
+++ php/index.php
@@ -10,6 +10,8 @@
 	<h1>Create an account</h1>
     </div>
     <div class="end">
+	<!-- Users and administrators are separated from the main page by choosing ‘admin login’ or ‘user login’ -->
+	<!-- Users cannot log into admin page, or admins cannot log into user page -->
 	<a href="./adminLogin.php"><input type="button" value="Admin Login"></a>
 	
 	<a href="./userLogin.php"><input type="button" value="User Login"></a>
