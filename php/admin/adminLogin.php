9dfb09795062dab1ada294fdb66e9f4274a0797d
Hannah <baby999@uw.edu>
2018-04-01 14:07:00 -0400

    Add files for login and logout

---
+++ php/admin/adminLogin.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+	<form method="POST" action="adminLoginValidate.php">
+	Username: <input type="text" name="username">
+	Password: <input type="password" name="password">
+	<input type="submit" value="Login">
+	</form>
+	
+</body>
+</html>
