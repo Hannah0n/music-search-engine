9dd6232561478ccbe00d2c4e6eec8ad3992d9d9b
Hannah <baby999@uw.edu>
2018-04-01 13:56:13 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>create account</title>	
+	<link rel="stylesheet" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Amatic SC' rel='stylesheet'>
+</head>
+<body>
+	<div class="container">
+   <div class="begin">
+	<h1>Create an account</h1>
+    </div>
+    <div class="end">
+	<a href="./adminLogin.php"><input type="button" value="Admin Login"></a>
+	
+	<a href="./userLogin.php"><input type="button" value="User Login"></a>
+	<?php
+	echo "<br/>";
+	?>
+	<br><a href="./create_account.html">Doesn't have an acocunt?</a>
+</div></div>
+	
+</body>
+</html>
