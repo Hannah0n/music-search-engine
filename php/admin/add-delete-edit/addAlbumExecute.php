af877322a64eccb40c62b6dbe6e718d7bb3f4a4e
Hannah An <baby999@uw.edu>
2018-04-01 14:41:12 -0400

    Update addAlbumExecute.php

--- php/admin/add-delete-edit/addAlbumExecute.php
+++ php/admin/add-delete-edit/addAlbumExecute.php
@@ -3,7 +3,11 @@
 	require_once('dbconnect.php');
 	$albumtitle = $_POST['albumtitle'];
 	$albumcompany = $_POST['albumcompany'];
-	
+
+	/*Unlike song ID and artist ID which are varchar type, album IDs are integers,
+	so every time an album is added, I find the maximum ID number among the currently registered album IDs
+	and add 1 to it, and then give the ID number (i.e. max + 1) to the newly added album.*/
+
 	$sql = "SELECT MAX(albumid) as maxid FROM album;";
 	$result = $conn->query($sql);
 	$max = $result->fetch_assoc()['maxid'];
